f3c933239b3255ce60131b56903642360ab66d21
chris <c.kahlefendt@gmail.com>
2017-08-20 18:08:37 +0800

    working parameters of int, double and bool

--- include/Parameter.h
+++ include/Parameter.h
@@ -4,28 +4,33 @@
 #include <memory>
 #include <vector>
 #include <pangolin/pangolin.h>
+#include <boost/variant.hpp>
 
 namespace ORB_SLAM2 {
+
+// For new types of parameters add type here, a setValue below and all the stuff in ParameterManager
+typedef boost::variant<int, double, bool> ParameterTypes;
+
 class ParameterBase {
- public:
-  enum ParamType{
+ protected:
+  enum ParameterCategory{
      MINMAX,
-     BOOL
+     BOOL,
+     UNDEFINED
   };
 
-  virtual ~ParameterBase(){};
-  /*! TODO: This shouldn't all be returned as double
-   *  \todo This shouldn't all be returned as double
-   */
-  virtual const double getValue() const = 0;
-  virtual void setValue(const double& value) = 0;
-  virtual const double getMinValue() const = 0;
-  virtual const double getMaxValue() const = 0;
-  virtual const double getParamType() const = 0;
-  virtual const std::string& getName() const = 0;
-
- protected:
   static std::vector<ParameterBase*> parameters;
+
+ public:
+  virtual ~ParameterBase(){};
+  virtual const ParameterTypes getVariant() const { return 0.0; };
+  virtual void setValue(const int& value){} ;
+  virtual void setValue(const double& value){} ;
+  virtual void setValue(const bool& value){} ;
+  virtual const int getMinValue() const { return 0.0; };
+  virtual const int getMaxValue() const { return 0.0; };
+  virtual const ParameterCategory getCategory() const { return ParameterCategory::UNDEFINED; };
+  virtual const std::string getName() const { return "undefined"; };
 };
 
 
@@ -34,7 +39,7 @@ class Parameter : public ParameterBase {
  public:
 
   Parameter(const std::string& name, const T& value)
-      : paramType(ParamType::BOOL)
+      : mCategory(ParameterCategory::BOOL)
       , minValue(0)
       , maxValue(0)
       , mValue(value)
@@ -43,8 +48,8 @@ class Parameter : public ParameterBase {
     parameters.push_back(this);
   }
 
-  Parameter(const std::string& name, const T& value, const T&minValue, const T& maxValue)
-      : paramType(ParamType::MINMAX)
+  Parameter(const std::string& name, const T& value, const int& minValue, const int& maxValue)
+      : mCategory(ParameterCategory::MINMAX)
       , minValue(minValue)
       , maxValue(maxValue)
       , mValue(value)
@@ -55,16 +60,17 @@ class Parameter : public ParameterBase {
 
   virtual ~Parameter(){};
 
-  virtual const double getValue() const override { return mValue; }; // cannot return a const ref because implicitly casted
-  virtual void setValue(const double& value) override { mValue = static_cast<T>(value); };
-  virtual const double getMinValue() const override { return minValue; };
-  virtual const double getMaxValue() const override { return maxValue; };
-  virtual const double getParamType() const override { return paramType; };
-  virtual const std::string& getName() const override { return mName; };
+  virtual const T& getValue() const { return mValue; };
+  virtual const ParameterTypes getVariant() const override { return mValue; }; // cannot return a const ref because implicitly casted
+  virtual void setValue(const T& value) override { mValue = value; };
+  virtual const int getMinValue() const override { return minValue; };
+  virtual const int getMaxValue() const override { return maxValue; };
+  virtual const ParameterCategory getCategory() const override { return mCategory; };
+  virtual const std::string getName() const override { return mName; };
 
-  const ParamType paramType;
-  T minValue;
-  T maxValue;
+  const ParameterCategory mCategory;
+  int minValue;
+  int maxValue;
 
  protected:
   T mValue;
@@ -74,21 +80,23 @@ class Parameter : public ParameterBase {
 
 class ParameterManager : public ParameterBase {
   public:
-  typedef std::map<std::string, std::pair<ParameterBase*, pangolin::Var<double>*> > ParameterPairMap;
+  typedef boost::variant<pangolin::Var<int>*, pangolin::Var<double>*, pangolin::Var<bool>*> PangolinVariants;
+  typedef std::map<std::string, std::pair<ParameterBase*, PangolinVariants>> ParameterPairMap;
 
   static ParameterPairMap& createPangolinEntries(const std::string& panel_name)
   {
-    for(auto& param : parameters)
+    for(const auto& param : parameters)
     {
-      if (param->getParamType() == ParamType::BOOL)
-      {
-        pangolinParams[param->getName()] = std::make_pair(param, (new pangolin::Var<double>(
-                panel_name + "." + param->getName(), param->getValue())));
-      }
-      else if (param->getParamType() == ParamType::MINMAX)
-      {
-        pangolinParams[param->getName()] = std::make_pair(param, (new pangolin::Var<double>(
-                panel_name + "." + param->getName(), param->getValue(), param->getMinValue(), param->getMaxValue())));
+      switch (param->getVariant().which()) {
+        case 0: // int
+          createPangolinEntry<int>(param, panel_name);
+          break;
+        case 1: // double
+          createPangolinEntry<double>(param, panel_name);
+          break;
+        case 2: // bool
+          createPangolinEntry<bool>(param, panel_name);
+          break;
       }
     }
     return pangolinParams;
@@ -98,17 +106,58 @@ class ParameterManager : public ParameterBase {
   {
     for(ParameterPairMap::iterator it = pangolinParams.begin(); it != pangolinParams.end(); it++)
     {
-      auto& pango_var = it->second.second;
+      auto& pango_var_variant = it->second.second;
       auto& param = it->second.first;
-      if(pango_var->Get() != param->getValue())
-      {
-        param->setValue(pango_var->Get());
-        // std::cout << "param value of " << param->getName() <<" is: " << param->getValue() << std::endl;
+
+      const int& type = param->getVariant().which();
+
+      if (type == 0) {
+        auto& pango_var =  boost::get<pangolin::Var<int>* >(pango_var_variant);
+        auto& value = boost::get<int>(param->getVariant());
+        if(pango_var->Get() != value)
+        {
+          param->setValue(pango_var->Get());
+          std::cout << "param value of " << param->getName() <<" is: " << boost::get<int>(param->getVariant()) << std::endl;
+        }
+      }
+      else if (type == 1) {
+        auto& pango_var =  boost::get<pangolin::Var<double>* >(pango_var_variant);
+        auto& value = boost::get<double>(param->getVariant());
+        if(pango_var->Get() != value)
+        {
+          param->setValue(pango_var->Get());
+          std::cout << "param value of " << param->getName() <<" is: " << boost::get<double>(param->getVariant()) << std::endl;
+        }
+      }
+      else if (type == 2) {
+        auto& pango_var =  boost::get<pangolin::Var<bool>* >(pango_var_variant);
+        auto& value = boost::get<bool>(param->getVariant());
+        if(pango_var->Get() != value)
+        {
+          param->setValue(pango_var->Get());
+          std::cout << "param value of " << param->getName() <<" is: " << boost::get<bool>(param->getVariant()) << std::endl;
+        }
       }
     }
   }
 
   private:
+  template<typename T>
+  static void createPangolinEntry(ParameterBase* param, const std::string& panel_name)
+  {
+    if (param->getCategory() == ParameterCategory::BOOL)
+    {
+      pangolinParams[param->getName()] = std::make_pair(param, (new pangolin::Var<T>(
+              panel_name + "." + param->getName(), boost::get<T>(param->getVariant()))));
+    }
+    else if (param->getCategory() == ParameterCategory::MINMAX)
+    {
+      pangolinParams[param->getName()] = std::make_pair(param, (new pangolin::Var<T>(
+              panel_name + "." + param->getName(), boost::get<T>(param->getVariant()),
+              param->getMinValue(),param->getMaxValue())));
+    }
+  }
+
   static ParameterPairMap pangolinParams;
 };
 }
